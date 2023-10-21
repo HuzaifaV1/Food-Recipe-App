5ffb71683881b51e4fcae21bc6cbde680997f81b
ammarsaqib <ammarr.saqib@gmail.com>
2023-10-20 22:24:23 -0700

    added div and fixed api

--- JS/main.js
+++ JS/main.js
@@ -1,16 +1,18 @@
 let submit = document.querySelector("#search-btn");
+let id = 0;
 
 function recipe(id) {
-    fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) // using api
+    console.log("Before");
+    fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) // using api
         .then(response => response.json()) // Response converted to JSON file
         .then(data => {
-            console.log(data.strInstructions);
+            console.log(data.meals[0].strInstructions);
         })
+    console.log("After");
 }
 
 
 submit.addEventListener("click", function () {
-    console.log("Before")
     let ingrediantName = document.getElementById("Ingredient-name").value; // Ingrediant Name
     let mealname = document.getElementById("recipe"); // Display recipe
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediantName}`) // using api
@@ -20,12 +22,13 @@ submit.addEventListener("click", function () {
             data.meals.forEach(meal => {
                 // Concatenate the HTML for each meal
                 html += `
+                <div class="meal-image">
                     <img src="${meal.strMealThumb}" alt="food" width="100px">
-                    <h3>${meal.strMeal}</h3>`;
+                    <h3 class="mealname">${meal.strMeal}</h3>
+                    </div>`;
             });
-            recipe(52772);
+            // recipe(52772);
             // Set the innerHTML of the 'div' element with the concatenated HTML
-            console.log(data)
             mealname.innerHTML = html;
         });
 });
