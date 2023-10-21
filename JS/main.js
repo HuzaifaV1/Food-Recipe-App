7c153fa29bdc101fe0e8c45cd1780a27d30eed15
ammarsaqib <ammarr.saqib@gmail.com>
2023-10-20 22:06:21 -0700

    fixed the div

--- JS/main.js
+++ JS/main.js
@@ -1,44 +1,31 @@
 let submit = document.querySelector("#search-btn");
-let myapikey = config.apikey;
-// function created to get the recipe
-function recipe(mealname) {
-    $.ajax({
-        method: 'GET',
-        url: 'https://api.api-ninjas.com/v1/recipe?query=' + mealname,
-        headers: { 'X-Api-Key': `${myapikey}` },
-        contentType: 'application/json',
-        success: function (result) {
-            console.log(result);
-        },
-        error: function ajaxError(jqXHR) {
-            console.error('Error: ', jqXHR.responseText);
-        }
-    });
+
+function recipe(id) {
+    fetch(`www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) // using api
+        .then(response => response.json()) // Response converted to JSON file
+        .then(data => {
+            console.log(data.strInstructions);
+        })
 }
 
 
 submit.addEventListener("click", function () {
+    console.log("Before")
     let ingrediantName = document.getElementById("Ingredient-name").value; // Ingrediant Name
-    let recipe = document.getElementById("recipe"); // Display recipe
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediantName}`)
+    let mealname = document.getElementById("recipe"); // Display recipe
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediantName}`) // using api
         .then(response => response.json()) // Response converted to JSON file
         .then(data => {
             let html = ''; // Initialize an empty string to store the concatenated HTML
             data.meals.forEach(meal => {
                 // Concatenate the HTML for each meal
                 html += `
-                <div class="meal-img">
                     <img src="${meal.strMealThumb}" alt="food" width="100px">
-                </div>
-                <div class="meal-name">
-                    <h3>${meal.strMeal}</h3>
-                </div>
-                <div class="recipe">
-                    <h3>${recipe(meal.strMeal)}</h3>
-                </div>`;
+                    <h3>${meal.strMeal}</h3>`;
             });
+            recipe(52772);
             // Set the innerHTML of the 'div' element with the concatenated HTML
             console.log(data)
-            recipe.innerHTML = html;
+            mealname.innerHTML = html;
         });
 });
