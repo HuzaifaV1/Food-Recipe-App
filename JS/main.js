cdd06f8647b7a6a5dfb5f890a969603c3caefdce
ammarsaqib <ammarr.saqib@gmail.com>
2023-10-21 01:11:43 -0700

    getting recipe instructions

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,10 @@
 let submit = document.querySelector("#search-btn");
+let mealname;
 let mealnumber = -1;
+let eval_table;
+let arrayOfMeals;
+let permaData;
+
 
 function recipe(id) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) // using api
@@ -13,7 +18,7 @@ function recipe(id) {
 
 submit.addEventListener("click", function () {
     let ingrediantName = document.getElementById("Ingredient-name").value; // Ingrediant Name
-    let mealname = document.getElementById("recipe"); // Display recipe
+    let recipe = document.getElementById("recipe"); // Display recipe
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediantName}`) // using api
         .then(response => response.json()) // Response converted to JSON file
         .then(data => {
@@ -24,12 +29,49 @@ submit.addEventListener("click", function () {
                 html += `
                 <div class="meal-image">
                     <img src="${meal.strMealThumb}" alt="food" width="100px">
-                    <a class="mealname${mealnumber}" onclick="getId();">${meal.strMeal}</a>
+                    <a class="mealname" id="${mealnumber}" href="#">${meal.strMeal}</a>
                     </div>`;
-                console.log(mealnumber);
+                mealname = document.getElementsByClassName("mealname");
             });
+
             // recipe(52772);
             // Set the innerHTML of the 'div' element with the concatenated HTML
-            mealname.innerHTML = html;
+            recipe.innerHTML = html;
+            e();
+
+            arrayOfMeals = Array.prototype.slice.call(eval_table);
+            console.log(arrayOfMeals)
+            console.log(data);
+            dataArray(data.meals);
+
+            console.log(permaData);
+
+            printRecipe();
         });
+    console.log("Before Before Before Before" + mealname);
+    // mealname = document.getElementsByClassName("mealname")
+    // console.log("Before Before Before" + mealname[0]);
 });
+console.log(mealname);
+
+function loop() {
+    for (let i = 0; i < arrayOfMeals.length; i++) {
+        arrayOfMeals[i].addEventListener('click', function () {
+            recipe(permaData[i].idMeal);
+        })
+    }
+}
+
+function e() {
+    eval_table = document.getElementsByClassName('mealname');
+    console.log(eval_table, eval_table.length);
+};
+
+function dataArray(data) {
+    permaData = Array.prototype.slice.call(data);
+}
+
+function printRecipe() {
+    console.log(loop());
+    console.log(permaData.meals);
+}
