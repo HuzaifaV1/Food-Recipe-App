88393f9e21765a16888010320bd3e43999dfa003
ammarsaqib <ammarr.saqib@gmail.com>
2023-10-21 16:12:14 -0700

    sent all recipe

--- JS/main.js
+++ JS/main.js
@@ -8,6 +8,10 @@ let mealRecipe;
 let mealName;
 let mealCategory;
 let yLink;
+let ingrediants;
+let ingredientMeasure;
+let displayIngredient;
+let ingrediantName;
 
 function recipe(id) {
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`) // using api
@@ -22,15 +26,28 @@ function recipe(id) {
             let mealHeading = document.querySelector("#data-heading");
             let Category = document.querySelector("#data-category");
             let youtubeLink = document.querySelector("#data-youtube");
+            displayIngredient = document.querySelector("#data-ingredient");
             instruction.innerHTML = mealRecipe; // printing recipe instruction
             mealHeading.innerHTML = mealName; // printing mealname
             Category.innerHTML = mealCategory; // printing categorey
             youtubeLink.href = yLink; // printing youtubelink
+            console.log(displayIngredient.innerHTML);
+            for (let i = 1; i <= 20; i++) {
+                ingrediants = data.meals[0]['strIngredient' + i] // getting each ingrediant name
+                ingredientMeasure = data.meals[0]['strMeasure' + i] // getting each ingrediants measurement
+                if (ingrediantName.toLowerCase() == ingrediants.toLowerCase()) {
+                    // document.getElementById("data-ingredient").style.color = "red";
+                }
+                if (ingrediants != "") {
+                    document.querySelector("#data-ingredient").innerHTML += i + ". " + ingredientMeasure + " " + ingrediants + "<br>";
+                }
+            }
         })
 }
 
+// to see when they clicked teh search button
 submit.addEventListener("click", function () {
-    let ingrediantName = document.getElementById("ingredient-name").value; // Ingrediant Name
+    ingrediantName = document.getElementById("ingredient-name").value; // Ingrediant Name
     let recipe = document.getElementById("recipe"); // Display recipe
     fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingrediantName}`) // using api
         .then(response => response.json()) // Response converted to JSON file
@@ -45,7 +62,7 @@ submit.addEventListener("click", function () {
                     <img src="${meal.strMealThumb}" alt="food" width="100px">
                     <a class="mealname" id="${mealnumber}" href="#">${meal.strMeal}</a>
                     </div>`;
-                mealname = document.getElementsByClassName("mealname");
+                mealname = document.getElementsByClassName("mealname"); // getting all mealnames
             });
 
             // Set the innerHTML of the 'div' element with the concatenated HTML
@@ -67,12 +84,13 @@ function loop() {
                          <div class="close-btn" onclick="togglePopup()">&times;</div>
                          <h1 id="data-heading"></h1>
                          <h3 id="data-category">hello</h3>
+                         <h3 id="data-ingredient"></h3>
                          <h3>Instructions:</h3>
                          <p id="data">${recipe(cloneData[i].idMeal)}</p>
                          <h2><a id="data-youtube" href="">Watch Video</a></h2>                         
                      </div>            
              </div>`;
-            togglePopup();
+            togglePopup(); // calling function togglePopup
         })
     }
 }
